933e744efaf1c1b834a7c93f5cab2d07bf447e4e
quantal-quetzal <33294791+quantal-quetzal@users.noreply.github.com>
2020-04-11 21:38:35 +0200

    Profilnamen geändert

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function private() {
   send(
-    "xhr=1&sid=82e6e71d64dbd1c2&lang=de&no_sidrenew=&name=Privat&fonbook=0&out_num=400&num_selection=sel_nums&num_1=400&num_5=7152&num_6=9299161&idx=1&back_to_page=%2Ffon_devices%2Ffondevices_list.lua&popup_url=&button_save=&oldpage=%2Ffon_devices%2Fedit_dect_num.lua"
+    "xhr=1&sid=82e6e71d64dbd1c2&lang=de&no_sidrenew=&name=400&fonbook=0&out_num=400&num_selection=sel_nums&num_1=400&num_5=7152&num_6=9299161&idx=1&back_to_page=%2Ffon_devices%2Ffondevices_list.lua&popup_url=&button_save=&oldpage=%2Ffon_devices%2Fedit_dect_num.lua"
   );
   document.getElementById("private").classList.add("click");
   setTimeout(
     () => document.getElementById("private").classList.remove("click"),
-    100
+    400
   );
 }
 
 function work() {
   send(
-    "xhr=1&sid=82e6e71d64dbd1c2&lang=de&no_sidrenew=&name=Arbeit+%26+Privat&fonbook=0&out_num=9299162&num_selection=sel_nums&num_1=400&num_5=7152&num_6=9299161&num_7=9299162&idx=1&back_to_page=%2Ffon_devices%2Ffondevices_list.lua&popup_url=&button_save=&oldpage=%2Ffon_devices%2Fedit_dect_num.lua"
+    "xhr=1&sid=82e6e71d64dbd1c2&lang=de&no_sidrenew=&name=9299162&fonbook=0&out_num=9299162&num_selection=sel_nums&num_1=400&num_5=7152&num_6=9299161&num_7=9299162&idx=1&back_to_page=%2Ffon_devices%2Ffondevices_list.lua&popup_url=&button_save=&oldpage=%2Ffon_devices%2Fedit_dect_num.lua"
   );
   document.getElementById("work").classList.add("click");
   setTimeout(
     () => document.getElementById("work").classList.remove("click"),
-    100
+    400
   );
 }
 
